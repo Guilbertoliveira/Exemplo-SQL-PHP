18ebcfe3bdf013eff85b10fdd83bfaf2dd5eb446
Guilbert <guilbertoliveira@hotmail.com>
2019-04-19 14:52:01 -0300

    Criação de Consulta

---
+++ consulta.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once("conexao.php");
+
+
+mysqli_close($conexao);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Conexao banco de dados</title>
+    <link rel="stylesheet" href="css/index.css"> <!-- importando o css !-->
+</head>
+<body>
+
+<div class="container">
+    <nav>
+        <ul class="menu">
+            <a href="index.php"><li>Cadastro</li></a>
+            <a href="consulta.php"><li>Consultas</li></a>
+        </ul>
+    </nav>
+    <section>
+        <h1>consulta</h1>
+        <hr><br><br>
+
+        <?php
+
+        ?>
+
+
+        </form>
+    </section>
+</div>
+</body>
+</html>
